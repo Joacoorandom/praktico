fb7df1ed7ebb6bf42e76d12ef547283145308890
Joacoorandom <62301477+Joacoorandom@users.noreply.github.com>
2026-02-05 23:04:46 -0300

    Delete admin-server directory

--- admin-server/start.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env sh
-# Arranque autónomo: npm install + node index.js (para Pterodactyl sin SSH)
-cd "$(dirname "$0")"
-echo "[Praktico Admin] npm install..."
-npm install
-echo "[Praktico Admin] Iniciando servidor..."
-exec node index.js
